91a485548e5848ebec51bad1a475ced992b67f15
Vijay Nandwani <born2bgeeky@gmail.com>
2017-11-27 15:49:33 +0530

    Adding data preprocessing code in main.py

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ Created on Mon Nov 27 15:27:05 2017
 @author: vijaynandwani
 """
 
+"""
+To create sub directories of news with one news file
+
+with open('news', 'r') as f:
+    text = f.read()
+    news = text.split("\n\n")
+    count = {'sport': 0, 'world': 0, "us": 0, "business": 0, "health": 0, "entertainment": 0, "sci_tech": 0}
+    for news_item in news:
+        lines = news_item.split("\n")
+        print(lines[6])
+        file_to_write = open('data/' + lines[6] + '/' + str(count[lines[6]]) + '.txt', 'w+')
+        count[lines[6]] = count[lines[6]] + 1
+        file_to_write.write(news_item)  # python will convert \n to os.linesep
+        file_to_write.close()
+"""
+
 import pandas
 import glob
 import pickle
